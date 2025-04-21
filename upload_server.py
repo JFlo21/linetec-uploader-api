537af44572e8c931f2b7289f0096124658f600f5
Juan Flores <juflores@ltspower.com>
2025-04-21 16:01:55 -0500

    Added coding path

--- upload_server.py
+++ upload_server.py
@@ -14,7 +14,14 @@ def upload():
         if not isinstance(data, list):
             return jsonify({"error": "Expected a list of records"}), 400
 
-        conn = psycopg2.connect("Cognos_Database")
+        conn = psycopg2.connect(
+    dbname="cognos_database",
+    user="cognos_database_user",
+    password="Y6b40zirOK7B0BZCETcruwqcLQXTCibE",
+    host="dpg-d019vqbuibrs73ahvbhg-a.virginia-postgres.render.com",
+    port=5432,
+    sslmode="require"
+)
 
         cursor = conn.cursor()
 
